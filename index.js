2700a07bc87c402539b265f3a5f43ef71443b783
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-03 20:11:48 +0700

    sdfsdf69

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors=require('cors')
 const app = express()
-//const {client}=require('./database/db')
+const {client}=require('./database')
 const http = require('http')
 const server =http.createServer(app);
 
