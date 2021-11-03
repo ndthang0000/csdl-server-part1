643b14ca556bfa89060774fb4f053798bc63f1e0
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-03 20:12:52 +0700

    6566

--- index.js
+++ index.js
@@ -14,46 +14,46 @@ app.use(cors())
 
 
 
-// app.get('/api/getbill/:year',async (req, res)=> {
-//   var year=parseInt(req.params.year)
-//   console.log(year)
-//   try {
-//     return await client.query(`SELECT * from hoadon h where EXTRACT(YEAR FROM ngaylap)=${year}`,(err,data)=>{
-//       res.status(200).json({result:data.rows,success:true})
-//     })
-//   }catch(err){
-//     res.status(400).json({error:"loi",success:false})
-//   }
-// })
-// app.get('/api/getbill', async(req, res)=> {
-//   try{
-//     return await client.query(`SELECT * FROM HOADON`,(err,data)=>{
-//       console.log(data.rows)
-//       res.status(200).json({result:data.rows,success:true})
-//     })
-//   }catch(err){
-//     res.status(400).json({error:"loi",success:false})
-//   } 
-// })
-// app.post('/api/add',async(req,res)=>{
-//   const data=req.body
-//   data.sum=parseFloat(data.sum)
-//   console.log(data)
+app.get('/api/getbill/:year',async (req, res)=> {
+  var year=parseInt(req.params.year)
+  console.log(year)
+  try {
+    return await client.query(`SELECT * from hoadon h where EXTRACT(YEAR FROM ngaylap)=${year}`,(err,data)=>{
+      res.status(200).json({result:data.rows,success:true})
+    })
+  }catch(err){
+    res.status(400).json({error:"loi",success:false})
+  }
+})
+app.get('/api/getbill', async(req, res)=> {
+  try{
+    return await client.query(`SELECT * FROM HOADON`,(err,data)=>{
+      console.log(data.rows)
+      res.status(200).json({result:data.rows,success:true})
+    })
+  }catch(err){
+    res.status(400).json({error:"loi",success:false})
+  } 
+})
+app.post('/api/add',async(req,res)=>{
+  const data=req.body
+  data.sum=parseFloat(data.sum)
+  console.log(data)
 
-//   try{
-//     return await client.query(`INSERT INTO HOADON("mahd","makh","ngaylap","tongtien") values ('${data.mahoadon}','${data.makhachhang}','${data.ngaylap}',${data.sum}); `,(err,data)=>{
-//       if(data){
-//         res.status(200).json({success:true})
-//       }else{
-//         res.status(200).json({success:false,message:'khachhang'})
-//       }
-//     })
-//   }catch(err){
-//     res.status(400).json({error:"loi",success:false})
+  try{
+    return await client.query(`INSERT INTO HOADON("mahd","makh","ngaylap","tongtien") values ('${data.mahoadon}','${data.makhachhang}','${data.ngaylap}',${data.sum}); `,(err,data)=>{
+      if(data){
+        res.status(200).json({success:true})
+      }else{
+        res.status(200).json({success:false,message:'khachhang'})
+      }
+    })
+  }catch(err){
+    res.status(400).json({error:"loi",success:false})
 
-//   }
+  }
   
-// })
+})
 app.get('/',(req,res)=>{
   res.sendFile(__dirname+'/index.html')
 })
